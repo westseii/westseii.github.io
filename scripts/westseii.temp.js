d1432a0d53866613b7e627c28ed38b121a29c051
Scott <66497481+westseii@users.noreply.github.com>
2020-06-18 00:18:00 -0700

    going to sleep now

---
+++ scripts/westseii.temp.js
@@ -0,0 +1,13 @@
+const project = {
+  name: "Project",
+  userName: "User",
+  version: "a.1.0",
+};
+
+const legalHTML = `
+<p>&copy; 2020 ${project.name}</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique sed quisquam perferendis repellat atque, quam accusantium!</p>
+<p>${project.version}</p>
+`;
+
+export { project, legalHTML };
