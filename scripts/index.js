cb63746ffc166c8bef536318f524177b7679141a
Scott <66497481+westseii@users.noreply.github.com>
2020-06-19 13:39:22 -0700

    cleanup and css fixes

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 import { project, legalHTML } from "./westseii.temp.js";
 
+//
 // sel
 const header = document.querySelector("header");
 const nav = document.querySelector("nav");
@@ -11,34 +12,46 @@ const pageTitle = document.querySelector(".page-title");
 const userGreet = document.querySelector(".user-greet");
 const legalContent = document.querySelector(".legal");
 
+//
 const btnToysToggleColors = document.querySelector(".ti-btn-colors");
 
+//
 // code
-document.title = `${project.name} - ${project.version}`;
+document.title = project.name;
+
 pageTitle.innerHTML = project.name;
 userGreet.innerHTML = `Hello, ${project.userName}`;
 legalContent.innerHTML = legalHTML;
 
-// toggle colors
+//
+// toys - toggle colors
 let colorsEnabled = false;
+
 function toggleColors() {
   colorsEnabled ? disableColors() : enableColors();
 }
+
 function enableColors() {
   colorsEnabled = true;
   _toggleColors();
 }
+
 function disableColors() {
   colorsEnabled = false;
   _toggleColors();
 }
+
+const toggleColorsArr = [
+  { element: header, attribute: "bg-tc1" },
+  { element: nav, attribute: "bg-tc2" },
+  { element: pageContent, attribute: "bg-tc3" },
+  { element: pageNotif, attribute: "bg-tc4" },
+  { element: footer, attribute: "bg-tc1" },
+];
+
 function _toggleColors() {
-  header.classList.toggle("bg-tc1");
-  nav.classList.toggle("bg-tc2");
-  pageContent.classList.toggle("bg-tc3");
-  pageNotif.classList.toggle("bg-tc4");
-  footer.classList.toggle("bg-tc1");
+  for (const obj of toggleColorsArr)
+    obj.element.classList.toggle(obj.attribute);
 }
 
-// events
 btnToysToggleColors.addEventListener("click", toggleColors);
